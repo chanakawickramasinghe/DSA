04a9bb45cccf1b9c8c16c34c70858752345e34a9
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2020-01-04 23:52:54 +0530

    Queue Implementation
    Implementation of a simple Queue without using pointers

---
+++ Queue/QueueImplementation.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+#define max 10 //define maximum size as 10
+
+int queue[max]; //initialize the queue
+int front = -1; rear = -1; // use to variables for the front and rear of the queue
+
+/*Function to check whether the Queue is Full or not
+This returns the vales 1 & 0 if the queue is full or not respectively*/
+int isFull(){
+    if (rear == max-1) //check whether the last value of the queue is equal to the maximum number -1 where it indicates a full queue
+	{
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+/*Function to check whether the Queue is Empty or not
+This returns the vales 1 & 0 if the queue is empty or not respectively*/
+int isEmpty (){
+    if (rear == -1) //check whether the firt value of the queue is equal to -1 where it indicates an empty queue
+	{
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+/*Function to insert elements into the queue*/
+int enqueue (){
+    int num;
+    if (isFull()){
+        printf("Queue is Full \n");
+    }
+    else {
+        if (front == -1){ //if the queue is empty make the front as the first element
+            front = 0;
+        }
+        printf("Inset the element in queue : ");
+        scanf("%d", &num);
+        rear = rear + 1;
+        queue[rear]=num;
+    }
+}
+
+/*Function to delete elements from the queue*/
+int dequeue () {
+    if (isEmpty() || front>rear){
+        printf("Queue is Empty \n");
+    }
+    else{
+        return queue[front++]; //increase the front and returns the stack with the next front value
+    }
+} 
+
+/*Function to print the queue*/
+int display (){
+    int i;
+    for (i=front; i<=rear; i++){ //printing the queue from front to rear
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
+}
+
+/*Main Function*/
+int main (){  
+    int n;  
+    while (1){//creating a loop until user wants to end the program
+    	printf("1. Enqueue \n");
+    	printf("2. Dequeue \n");
+    	printf("3. Display \n");
+    	printf("4. Exit \n");
+    	
+    	printf("Enter your choice : ");
+    	scanf("%d",&n);
+    	
+    	switch (n){//calling fumctions using switch case
+    		case 1:enqueue();
+    				break;
+    		case 2:dequeue();
+    				break;
+    		case 3:display();
+    				break;
+    		case 4:exit(0);
+		}
+	}
+	return 0;  
+}
